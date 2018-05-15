6bf78e2ec22a19233f114a154b0e5ae70c7979fa
fananchong <fananchong@126.com>
2018-05-15 14:05:28 +0800

    add Dockerfile

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
 #/bin/bash
 
+set -ex
+
+set CURPATH=$PWD
+
 docker run -it --rm -v "$PWD":/go/src/whatsmyip -w /go/src/whatsmyip golang go build .
+
+mv -f whatsmyip docker/
+
+cd docker
+
+docker build -t fananchong/whatsmyip .
+
+rm -f whatsmyip
+
+cd $CURPATH
